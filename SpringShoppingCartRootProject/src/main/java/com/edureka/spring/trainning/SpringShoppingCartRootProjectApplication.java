ecd1c6b7bf63293ebb998ae6321434a11765cde7
saumen das <63276757+saumen-das1984@users.noreply.github.com>
2020-04-28 00:28:10 +0530

    Add files via upload
    root code

---
+++ SpringShoppingCartRootProject/src/main/java/com/edureka/spring/trainning/SpringShoppingCartRootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.edureka.spring.trainning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShoppingCartRootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShoppingCartRootProjectApplication.class, args);
+	}
+
+}
---
+++ SpringShoppingCartRootProject/src/test/java/com/edureka/spring/trainning/SpringShoppingCartRootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edureka.spring.trainning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShoppingCartRootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
